1d7cb20e7ea632fe3003f3fc68b224d69475f028
Eman_Shehata <emanshehata258@gmail.com>
2026-01-06 09:57:29 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from "./pages/login/login.component";
+import { SideBarComponent } from "./layouts/side-bar/side-bar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, RegisterComponent, LoginComponent, SideBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
