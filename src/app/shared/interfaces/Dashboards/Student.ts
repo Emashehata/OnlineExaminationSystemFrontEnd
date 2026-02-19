e4bfac26000623f6ecb097afbac30ed1706ee0df
rawda ashour <ashrawda@gmain.com>
2026-02-19 17:44:06 +0200

    Final Updates BGAD

--- src/app/shared/interfaces/Dashboards/Student.ts
+++ src/app/shared/interfaces/Dashboards/Student.ts
@@ -10,16 +10,22 @@ export interface StudentDashboardData {
 }
 
 export interface PerformanceBySubject {
+  averageScore: any;
+  courseName: any;
   subject: string;
   score: number;
 }
 
 export interface ScoreTrend {
+  averageScore: any;
+  monthName: any;
   month: string;
   score: number;
 }
 
 export interface RecentResult {
+  submittedAt: string | number | Date;
+  courseName: any;
   subject: string;
   date: string;
   score: number;
